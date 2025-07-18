5f1b61eb96c4b9c7a2858e3d58ad2a31eaf04611
chikichiki12 <137501960+chikichiki12@users.noreply.github.com>
2025-07-18 13:59:25 +0300

    declared and initialized all the necessary variables

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,41 @@ namespace SnakeGame
 {
     public partial class Form1 : Form
     {
+        Random rand;
+
+        enum gameBoardFields
+        {
+            empty,
+            snake,
+            food
+        };
+
+        enum direction
+        {
+            up,
+            down,
+            left,
+            right
+        };
+
+        struct snakeCoordinates
+        {
+            public int x;
+            public int y;
+        };
+
+        gameBoardFields[,] gameBoard;
+        snakeCoordinates[] snakeXY;
+        int snakeLength;
+        direction snakeDirection;
+        Graphics graphics;
+
         public Form1()
         {
             InitializeComponent();
+            gameBoard = new gameBoardFields[11, 11];
+            snakeXY = new snakeCoordinates[100];
+            rand = new Random();
         }
 
         private void Form1_Load(object sender, EventArgs e)
